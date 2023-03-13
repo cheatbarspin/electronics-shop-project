f5c8a536cf039b8d14ec9ac93a22cce5b43522da
Daniels Ivaskevics <wavesaftwaves@gmail.com>
2023-03-13 22:53:43 +0100

    added tests with 96% coverage

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-from src import item
 import pytest
 
+from src.item import Item
+
+
 @pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_item_init(item1):
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+    assert item1.quantity == 20
+
+
+def test_calculated_total_price(item1):
+    assert item1.calculate_total_price() == 200000
+
 
